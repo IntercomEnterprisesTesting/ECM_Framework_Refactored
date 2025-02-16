f956ef42da3bf7a7e1964c93aa078d35e30648a0
IntercomEnterprisesTesting <Mazen.yousri@intercom.com.eg>
2025-02-16 17:13:33 +0200

    added clear folder method

--- src/tests/TestBase.ts
+++ src/tests/TestBase.ts
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+/* eslint-disable no-restricted-syntax */
 import {
      test as base, Browser, BrowserContext, Page,
     } from '@playwright/test';
@@ -6,10 +8,11 @@ import HomePageSteps from '@uiSteps/HomePageSteps';
 import UIActions from '@uiActions/UIActions';
 import UsersReader from 'Excel/usersReader';
 import FolderNavigationUtil from '@utils/FolderNavigationUtil';
-import { Folder, DocumentClass } from 'Excel/types';
+import { Folder } from 'Excel/types';
 import DataBuilder from 'Excel/DataBuilder';
 import AddDocumentPageSteps from '@uiSteps/AddDocumentPageSteps';
 import AttributeUtil from '@utils/AttributeUtil';
+import HomePage from '@pages/HomePage';
 
 export default class TestBase {
      browser: Browser;
@@ -42,10 +45,28 @@ export default class TestBase {
             this.addDocument = new AddDocumentPageSteps(this.uiActions, this.attributeUtil);
             this.homeSteps = new HomePageSteps(this.uiActions, this.folderNavigationUtil, this.addDocument);
         });
-
-        // base.afterAll(async () => {
-        //     await this.page.close();
-        //     await this.context.close();
-        // });
     }
+
+    public async clearFolders() {
+        const docs = this.excel.getAllDocumentTypes();
+        console.log(`docs = ${docs}`);
+        for (const doc of docs) {
+            try {
+                await this.folderNavigationUtil.navigateToFolder(doc.folderName);
+                const alertIsVisible = await this.uiActions.element(HomePage.FOLDER_IS_EMPTY_TEXT, "Folder is empty text").isVisible();
+                if (!alertIsVisible) {
+                    await this.homeSteps.deleteUploadedFiles();
+                } else {
+                    return;
+                }
+            } catch (error) {
+                console.log(`Error: ${error.message}`); 
+            }
+        }
+    }
+
+    // base.afterAll(async () => {
+    //     await this.page.close();
+    //     await this.context.close();
+    // });
 }
